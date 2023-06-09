a0644bcc5d95a71522a1ef4cd843aab47a2112a7
Christian <chrisnonymous2000@gmail.com>
2023-06-08 23:39:03 -0600

    add vita support from https://github.com/vitasdk/packages/blob/master/physfs

--- src/physfs_platforms.h
+++ src/physfs_platforms.h
@@ -69,6 +69,9 @@
 #elif defined(__QNX__)
 #  define PHYSFS_PLATFORM_QNX 1
 #  define PHYSFS_PLATFORM_POSIX 1
+#elif defined(__vita__)
+#  define PHYSFS_PLATFORM_VITA 1
+#  define PHYSFS_NO_CDROM_SUPPORT 1
 #elif defined(unix) || defined(__unix__)
 #  define PHYSFS_PLATFORM_UNIX 1
 #  define PHYSFS_PLATFORM_POSIX 1
